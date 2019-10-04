399cbc7ae96717b9d18cccbac1039fa61631d32e
Fehim Mahfoud <fehim.mahfoud19@gmail.com>
2019-10-04 22:14:10 +0100

    Ajoue Import et lecture nombre sommet au niveau de la console

--- Algorithms/Graphs/Dijkstra's Algorithm/Java/Dijkstra.java
+++ Algorithms/Graphs/Dijkstra's Algorithm/Java/Dijkstra.java
@@ -6,11 +6,35 @@
  */
 package road;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+
 /**
  *
  * @author Mahfoud
  */
 public class Dijkstra {
 
-	
+			System.out.print("Dijikstra");
+		Scanner scan = new Scanner(System.in);
+
+		ArrayList<String> S_prime = new ArrayList<>();
+		ArrayList<String> S = new ArrayList<>();
+
+		int[] phi;
+		int nbSommet;
+
+		int[][] matriceIncidence;
+		int[][] poidArc;
+
+		System.out.print("\n Entrer Nombre de sommet :");
+		nbSommet = scan.nextInt();
+
+		phi = new int[nbSommet];
+
+		matriceIncidence = new int[nbSommet][nbSommet];
+		poidArc = new int[nbSommet][nbSommet];
+
 }
