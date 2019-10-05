b09cddff32537575eda688539d264cfb062f9472
jahnvigupta <43217070+jahnvigupta@users.noreply.github.com>
2019-10-05 18:21:36 +0530

    Update selectionsort.py

--- Algorithms/Searching & Sorting/Selection Sort/selectionsort.py
+++ Algorithms/Searching & Sorting/Selection Sort/selectionsort.py
@@ -1,22 +1,24 @@
-def selectionsort(arr):
-	i=0
-	while(i<len(arr)):
-		#find index of minimum number between index i and index n-1 where n=len(arr)
-		num,ind=arr[i],i
-		for j in range(i+1,len(arr)):
-			if(arr[j]<num):
-				num=arr[j]
-				ind=j
-		#swap number at ind with number at i
-		temp=arr[i]
-		arr[i]=arr[ind]
-		arr[ind]=temp
-		i+=1
-	return arr
-
-def main():
-	arr = [9,7,4,3,1,5,3,6,7,0]
-	sorted_arr = selectionsort(arr)
-	print(sorted_arr)
-if __name__ == "__main__":
-	main()
+import sys 
+A = []
+n = int(input())
+for i in range(n):
+	A.append(int(input()))
+  
+# Traverse through all array elements 
+for i in range(len(A)): 
+      
+    # Find the minimum element in remaining  
+    # unsorted array 
+    min_idx = i 
+    for j in range(i+1, len(A)): 
+        if A[min_idx] > A[j]: 
+            min_idx = j 
+              
+    # Swap the found minimum element with  
+    # the first element         
+    A[i], A[min_idx] = A[min_idx], A[i] 
+  
+# Driver code to test above 
+print ("Sorted array") 
+for i in range(len(A)): 
+    print("%d" ,%A[i])
