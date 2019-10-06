ee41d5b4889f72f0110907cb14b125a3c9c33463
Fehim Mahfoud <fehim.mahfoud19@gmail.com>
2019-10-06 13:04:57 +0100

    Adding isPalindrome Function add two exmeple

--- Algorithms/Maths/Pallindrome number/Java/palindrome.java
+++ Algorithms/Maths/Pallindrome number/Java/palindrome.java
@@ -1,8 +1,32 @@
 
 class palindrome {
 
+    public boolean isPalindrome(Integer number){
+        int start = 0;
+        int end = number.toString().length()-1;
+        boolean result = false;
+
+        String [] numberStr = number.toString().split("");
+
+        while(start < end){
+            if(numberStr[start].equals(numberStr[end])){
+                result = true;
+            }else{
+                result = false;
+            }
+            start++;
+            end--;
+        }
+
+        return result;
+    }
+
 
     public static void main(String[] args) {
+        Palindrome palindrome = new Palindrome();
+
+		System.out.println("Result False " + palindrome.isPalindrome(1231));
+		System.out.println("Result True " + palindrome.isPalindrome(12321));
 
 
     }
