1539a4e0af5460c44c93040181bca7af2ce5390e
rishabhsingh20 <kumarrishabh2008@gmail.com>
2019-10-06 18:30:57 +0530

    String Palindrome

---
+++ string.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char string1[20];
+    int i, length;
+    int flag = 0;
+    
+    cout << "Enter a string: "; cin >> string1;
+    
+    length = strlen(string1);
+    
+    for(i=0;i < length ;i++){
+        if(string1[i] != string1[length-i-1]){
+            flag = 1;
+            break;
+   }
+}
+    
+    if (flag) {
+        cout << string1 << " is not a palindrome" << endl; 
+    }    
+    else {
+        cout << string1 << " is a palindrome" << endl; 
+    }
+    system("pause");
+    return 0;
+}
