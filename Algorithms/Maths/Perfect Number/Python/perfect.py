0b1d388dbf724048b6c01eaeedf17030adb3fe19
Patric Dexheimer <patric.dexheimer@gmail.com>
2019-10-06 14:06:38 -0300

    Adjusting example
    Making it more idiomatic. 
    Converted user input to int.

--- Algorithms/Maths/Perfect Number/Python/perfect.py
+++ Algorithms/Maths/Perfect Number/Python/perfect.py
@@ -1,15 +1,18 @@
-print( "Enter Perfect Number interval:" )
+def main():
+	print( "Enter Perfect Number interval:" )
 
-start = input("Start:")
-end   = input("End:")
+	start = int(input("Start:"))
+	end   = int(input("End:"))
 
-for number in range(start, end):
-	accum = 0
+	for number in range(start, end):
+		accum = 0
 
-	for index in range(1 , number):
-		if number % index == 0:
-			accum =  accum + index
+		for index in range(1 , number):
+			if number % index == 0:
+				accum =  accum + index
 
-	if accum == number:
-		print(number)
+		if accum == number:
+			print(number)
 			
+if __name__ == '__main__':
+    main()
