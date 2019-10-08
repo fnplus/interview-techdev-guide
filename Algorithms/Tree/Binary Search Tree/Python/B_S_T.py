9f3e4df1863d63121c6b08125c4e823912450177
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-10-09 02:48:11 +0530

    added search function in B_S_T.py

--- Algorithms/Tree/Binary Search Tree/Python/B_S_T.py
+++ Algorithms/Tree/Binary Search Tree/Python/B_S_T.py
@@ -15,10 +15,10 @@ class BinarySearchTree:
     
     def insert_into_tree(self, value):
         newNode = Node(value=value)
-        root = self.root
-        if root is None:
+        if self.root is None:
             self.root = newNode
         else:
+            root = self.root
             while root:
                 if value < root.value:
                     if root.left is None:
@@ -32,7 +32,27 @@ class BinarySearchTree:
                         break
                     else:
                         root = root.right
-            
+
+    def search_in_tree(self,value):
+        if self.root is None:
+            return f'value({value}) not found'
+        elif self.root.value == value:
+            return f'value({value}) found in root'
+        else:
+            root = self.root
+            while root:
+                if value == root.value:
+                    return f'value({value}) found in tree'
+                elif value < root.value:
+                    if root.left is None:
+                        return f'value({value}) not found'
+                    else:
+                        root = root.left
+                else:
+                    if root.right is None:
+                        return f'value({value}) not found'
+                    else:
+                        root = root.right
     
     def inorder_traversal(self, root):
         if root:
@@ -50,3 +70,7 @@ if __name__ == '__main__':
     BST.insert_into_tree(value=3)
     # Inorder traversal of the tree
     BST.inorder_traversal(root=BST.root)
+    print() # Just for a newline :)
+    # Searching for a value in the tree
+    print(BST.search_in_tree(value=12))
+    print(BST.search_in_tree(value=14))
