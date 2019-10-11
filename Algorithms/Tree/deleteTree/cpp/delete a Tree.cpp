bc30d55543090f8c0d9306a01ab522aa512b81b3
NripeshKumar <nripesh1221@gmail.com>
2019-10-11 18:58:48 +0530

    delete a tree

--- Algorithms/Tree/deleteTree/cpp/delete a Tree.cpp
+++ Algorithms/Tree/deleteTree/cpp/delete a Tree.cpp
@@ -1,20 +1,13 @@
 #include<bits/stdc++.h>  
 #include<iostream> 
 using namespace std;  
-  
-/* A binary tree node has data, 
-pointer to left child and  
-a pointer to right child */
+
 class node  
 {  
     public: 
     int data;  
     node* left;  
     node* right;  
-      
-    /* Constructor that allocates  
-    a new node with the given data 
-    and NULL left and right pointers. */
     node(int data) 
     { 
         this->data = data;  
@@ -24,24 +17,17 @@ class node
 };  
   
   
-/* This function traverses tree  
-in post order to delete each  
-and every node of the tree */
 void deleteTree(node* node)  
 {  
     if (node == NULL) return;  
   
-    /* first delete both subtrees */
     deleteTree(node->left);  
     deleteTree(node->right);  
       
-    /* then delete the node */
     cout << "\n Deleting node: " << node->data;  
     free(node);  
 }  
   
-  
-/* Driver code*/
 int main()  
 {  
     node *root = new node(1);  
@@ -58,4 +44,3 @@ int main()
     return 0;  
 }  
   
-//This code is contributed b
