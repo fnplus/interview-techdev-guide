2fbac811681489bfba85b6864691c90aa67d8d60
Uniyal <abhishekuniyal09@gmail.com>
2019-10-23 10:11:12 +0530

    cleanup

--- Algorithms/Searching & Sorting/Bubble Sort/bubble_sort.c
+++ Algorithms/Searching & Sorting/Bubble Sort/bubble_sort.c
@@ -1,55 +1,34 @@
-// below we have a simple C program for bubble sort
-#include <stdio.h>
+#include<stdio.h>
 
-void bubbleSort(int arr[], int n)
-{
-    int i, j, temp;
-    for(i = 0; i < n; i++)
-    {
-        for(j = 0; j < n-i-1; j++)
-        {
-            // introducing a flag to monitor swapping
-            int flag = 0;
-            if( arr[j] > arr[j+1])
-            {
-                // swap the elements
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                // if swapping happens update flag to 1
-                flag = 1;
-            } 
-        }
-        // if value of flag is zero after all the iterations of inner loop
-        // then break out
-        if(!flag)
-        {
-            break;
-        }
-    }
-    
-    // print the sorted array
-    printf("Sorted Array: ");
-    for(i = 0; i < n; i++)
-    {
-        printf("%d  ", arr[i]);
-    }
-}
-
-int main()
-{
-    int arr[100], i, n, step, temp;
-    // ask user for number of elements to be sorted
-    printf("Enter the number of elements to be sorted: ");
+int main() {
+    int n, arr[100];
+    printf("Enter no of elements: ");
     scanf("%d", &n);
-    // input elements if the array
-    for(i = 0; i < n; i++)
-    {
-        printf("Enter element no. %d: ", i+1);
+    printf("Enter %d elements now: ", n);
+    for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    // call the function bubbleSort
-    bubbleSort(arr, n);
-    
-    return 0;
+
+    printf("Before the sort: \n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    //Bubble sort algo
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                //Swapping the elements
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    printf("\nAfter the sort: \n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
