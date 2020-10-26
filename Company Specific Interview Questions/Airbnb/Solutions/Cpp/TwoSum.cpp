0e8541942b3034b39d8a15513f478fd42f0baadf
sahilsingh2402 <44718639+sahilsingh2402@users.noreply.github.com>
2020-10-26 08:32:54 +0530

    Made Changes To Improve The Quality Of Codes

--- Company Specific Interview Questions/Airbnb/Solutions/Cpp/TwoSum.cpp
+++ Company Specific Interview Questions/Airbnb/Solutions/Cpp/TwoSum.cpp
@@ -1,51 +1,52 @@
-/*
-Contributed by Bipul Kumar 
-https://github.com/imbipulkumar/
-*/
+//TwoSum
+//Given a list of numbers and a number k, return indices of two numbers from the list add up to k.
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include<map>
+
 using namespace std;
 
-// TwoSum() returns array of size 2 containing required indices of the two numbers.
-int* TwoSum(int* a,int n,int x)
-{
-    unordered_map<int,int> complement;
-    /*
-	Iterate and insert elements into the hash table, we also look back to check if current element's complement already exists in the table. 
-	If it exists, we have found a solution and return immediately.
-	*/
-    for(int i=0;i<n;i++)
-    {
-        auto itr=complement.find(x-a[i]);
-        if(itr!=complement.end())
-        {
-            return new int[2] {itr->second,i};
+vector<int> twoSum(vector<int>& nums, int target) {
+    map <int, int> mp;
+    vector<int> result;
+    int rem;
+
+    for(int i = 0; i < nums.size(); i++) {
+        rem = target - nums[i];
+        if(mp.find(rem) != mp.end() && mp.find(rem)->second != i) {
+            result.push_back(mp.find(rem)->second);
+            result.push_back(i);
+
+            return result;
         }
-        else
-        {
-            complement.insert({a[i],i});
-        }
+        mp.insert(pair<int, int>(nums[i], i));
     }
-    throw "No such two numbers";
+    return result;
 }
 
-// Driver program to test above function.
+// Input format: n (number of terms in array), target
+// 			  	 nums(array of length n)
+// Output: Indices of numbers that give the required target sum
+
 int main() {
-    int n,x;
-    // n=size of array a and x=target sum.
-    cin>>n>>x;
-    int a[n];
-    for(int i=0;i<n;i++)
-    {
-        cin>>a[i];
-    }
-    int *index=TwoSum(a,n,x);
-    cout<<index[0]<<" "<<index[1]<<endl;
-    return 0;
-}
+	int t;
+	cin >> t;
 
-/*
-Complexity Analysis:
-Time complexity : O(n). We traverse the list containing nn elements only once. Each look up in the table costs only O(1) time.
-Space complexity : O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nn elements.
-*/ 
+	while(t--) {
+		vector<int> nums;
+		int target, n;
+		cin >> n >> target;
+		nums.resize(n);
+		for(int i = 0; i < n; i++) {
+			cin >> nums[i];
+		}
+		vector<int> result = twoSum(nums, target);
+
+		if(result.size() == 2) {
+			cout << result[0] << " " << result[1] << "\n";
+		}
+		else 
+			cout << "No result found" <<"\n";
+	}
+}
